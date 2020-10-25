58a79cde4c29b80d993617f7789149bcf51996ec
Bhoomika Sood <bhoomikasood123@gmail.com>
2020-10-25 14:07:28 -0400

    recipe card

--- client/src/components/recipe.js
+++ client/src/components/recipe.js
@@ -19,24 +19,36 @@ class Recipe extends Component {
       );
   }
 
-  render() {
-    return (
-      <div>
-        <ul>
-          {this.state.recipes.map((recipe) => (
-            <li className="Recipes" key={recipe.idrecipe}>
-              <h1>
-                <p>ID: {recipe.idrecipe}</p>
-                <p>Recipe Name: {recipe.recipeName}</p>
-                <p>Instructions: {recipe.recipeInstruction}</p>
-                <p className="hello"> hello </p>
-              </h1>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+  
+render() {
+  return (
+ <div>
+<div class="card-container">
+  <div class="card u-clearfix">
+    <div class="card-body">
+    <div class="card-border">
+      <ul>
+      {this.state.recipes.map((recipe) => (
+        <li className="Recipes" key={recipe.idrecipe}>
+        <h1> 
+          <span class="card-number card-circle subtle">ID{recipe.idrecipe}</span>
+          <h2 class="card-title">{recipe.recipeName} </h2>
+          <div class="card-read"></div>
+          <span class="card-description subtle"> Instructions: 
+          <p>{recipe.recipeInstruction}</p> </span>
+        </h1>
+        </li>
+      ))}
+    </ul>
+    </div>
+    </div>
+    <img src="https://s15.postimg.cc/temvv7u4r/recipe.jpg" alt="" class="card-media" />
+  </div>
+  <div class="card-shadow"></div>
+</div>
+</div>   
+)}
+
 }
 
 export default Recipe;
