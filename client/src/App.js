87f4cddc794cb2f9df9407b8f5316489aa7b6534
Indusha <im2semba@uwaterloo.ca>
2020-10-26 11:43:22 -0400

    App.js changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import './App.css';
-import LoginButton from './components/LoginButton';
-import LogoutButton from './components/LogoutButton';
-import Profile from './components/Profile';
-import { useAuth0 } from '@auth0/auth0-react';
+import React from "react";
+import "./App.css";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/Profile";
+import { useAuth0 } from "@auth0/auth0-react";
+import Recipe from "./components/Recipe";
 
 function App() {
   const { isLoading } = useAuth0();
 
-  if (isLoading) return <div>Loading...</div>
+  if (isLoading) return <div>Loading...</div>;
 
   return (
     <>
       <LoginButton />
       <LogoutButton />
       <Profile />
+      <Recipe />
     </>
   );
 }
