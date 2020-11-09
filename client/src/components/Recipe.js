d0244a5e2c8571d45f2329b5f4deb6739f102a0b
Bhoomika Sood <bhoomikasood123@gmail.com>
2020-11-09 01:21:28 -0500

    explore UI

--- client/src/components/Recipe.js
+++ client/src/components/Recipe.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
+import AddRecipe from "./addRecipe";
 import "./Recipe.css";
+import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 class Recipe extends Component {
   constructor() {
@@ -21,26 +24,46 @@ class Recipe extends Component {
         )
       );
   }
+  /*isAuthenticated() {
+    var Authenticated = useAuth0();
+    this.state.isAuthenticated = Authenticated;
+  }*/
 
+  //added react route link in this component to open the edit recipe form with prefilled fields
   render() {
+   
     return (
-      <div>
-        <ul>
-          {this.state.recipes.map((
-            recipe //iterate through each recipe object in the state array display the id, name and instructions of each recipe
-          ) => (
-            <li className="Recipes" key={recipe.idrecipe}>
-              <h1>
-                <p>id :{recipe.idrecipe}</p>
-                <p>name : {recipe.recipeName}</p>
-                <p>instructions: {recipe.recipeInstruction}</p>
-              </h1>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <Router>
+       
+        <div>
+          <ul>
+            {this.state.recipes.map((
+              recipe //iterate through each recipe object in the state array display the id, name and instructions of each recipe
+            ) => (
+              <li className="Recipes" key={recipe.idrecipe}>
+                <Link to="/addRecipe" name="hello" instructions="jee;;p">
+                  Edit
+                </Link>
+                {/* <button type = "button" className="btn btn-primary btn-lg" onClick={() => console.log('hello')}>Edit</button> */}
+                <h1>
+
+                  <p>id :{recipe.idrecipe}</p>
+                  <p>name : {recipe.recipeName}</p>
+                  <p>instructions: {recipe.recipeInstruction}</p>
+
+                </h1>
+              </li>
+            ))}
+          </ul>
+          <Switch>
+            <Route path="/addRecipe">
+              <AddRecipe />
+            </Route>
+          </Switch>
+        </div>
+      
+      </Router>
     );
   }
 }
-
 export default Recipe; //Export the recipe component to be used in the main index.js file
