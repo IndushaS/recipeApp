c51277029ece019372a672853f3c32c7980e478a
defne <dcelebi@uwaterloo.ca>
2020-11-09 16:58:32 +0300

    updated column name for recipe images

--- server.js
+++ server.js
@@ -27,15 +27,15 @@ app.get("/hello", (req, res) => {
 
 //Display recipes on explore page UPDATED
 app.get("/getRecipes", (req, res, next) => {
-  connection.query("Select recipe.recipeName, recipe.img, GROUP_CONCAT(ingredients.name) as ingredients, recipe.recipeInstruction From recipeApp.recipe_ingredients Join recipeApp.recipe On recipe_ingredients.idrecipe = recipe.idrecipe Join recipeApp.ingredients On recipe_ingredients.recipe_ingredient = ingredients.idingredient GROUP BY recipe.recipeName, recipe.img, recipe.recipeInstruction ", function (error, results, fields) {
+  connection.query("Select recipe.idrecipe, recipe.recipeName, recipe.imgURL, GROUP_CONCAT(ingredients.name) as ingredients, recipe.recipeInstruction From recipeApp.recipe_ingredients Join recipeApp.recipe On recipe_ingredients.idrecipe = recipe.idrecipe Join recipeApp.ingredients On recipe_ingredients.recipe_ingredient = ingredients.idingredient GROUP BY recipe.idrecipe, recipe.recipeName, recipe.imgURL, recipe.recipeInstruction ", function (error, results, fields) {
     if (error) throw error;
     res.json(results);
   });
 });
 
 app.get('/api/addRecipe', (req, res) => {
-  const {idrecipe, recipeName, img, recipeInstruction} = req.query;
-  const Insert_Recipe = "INSERT INTO recipe (idrecipe, recipeName, img, recipeInstruction) VALUES ('" + idrecipe + "','" + recipeName + "','" + img + "','" + recipeInstruction + "')";
+  const {idrecipe, recipeName, imgURL, recipeInstruction} = req.query;
+  const Insert_Recipe = "INSERT INTO recipe (idrecipe, recipeName, imgURL, recipeInstruction) VALUES ('" + idrecipe + "','" + recipeName + "','" + imgURL + "','" + recipeInstruction + "')";
   connection.query(Insert_Recipe, (err, results) => {
     if (err) {
       return res.send(err)
@@ -49,7 +49,7 @@ app.get('/api/addRecipe', (req, res) => {
 //API call for saved recipes
 app.get('/api/saved_recipes', (req, res) => {
   const {iduser} = req.query;
-  const Saved_Recipes = "Select recipe.recipeName as recipeName, recipe.img as image From recipeApp.saved_recipe Join recipeApp.user On saved_recipe.id_user = user.iduser Join recipeApp.recipe On saved_recipe.recipe_saved = recipe.idrecipe Where iduser ="+ iduser ;
+  const Saved_Recipes = "Select recipe.recipeName as recipeName, recipe.imgURL as image From recipeApp.saved_recipe Join recipeApp.user On saved_recipe.id_user = user.iduser Join recipeApp.recipe On saved_recipe.recipe_saved = recipe.idrecipe Where iduser ="+ iduser ;
   connection.query(Saved_Recipes, (err, results) => {
     if (err) {
       return res.send(err)
@@ -63,7 +63,7 @@ app.get('/api/saved_recipes', (req, res) => {
 //API call for created recipes
 app.get('/api/created_recipes', (req, res) => {
   const {iduser} = req.query;
-  const Created_Recipes = "Select recipe.recipeName as recipeName, recipe.img as image From recipeApp.created_recipe Join recipeApp.user On created_recipe.userid = user.iduser Join recipeApp.recipe On created_recipe.recipe_created = recipe.idrecipe Where iduser ="+ iduser ;
+  const Created_Recipes = "Select recipe.recipeName as recipeName, recipe.imgURL as image From recipeApp.created_recipe Join recipeApp.user On created_recipe.userid = user.iduser Join recipeApp.recipe On created_recipe.recipe_created = recipe.idrecipe Where iduser ="+ iduser ;
   connection.query(Created_Recipes, (err, results) => {
     if (err) {
       return res.send(err)
