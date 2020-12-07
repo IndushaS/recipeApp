bf57a1bccf4fafe4ff2f1397b6ec2884b6844f03
Indusha <im2semba@uwaterloo.ca>
2020-12-07 02:27:52 -0500

    final app1

--- src/components/about.js
+++ src/components/about.js
@@ -3,12 +3,12 @@ import "./about.css";
 import logo from '../assets/chefhat.png'
 
 
-class About extends Component {  
-render() {
+class About extends Component {
+  render() {
     return (
       <div>
-        <div className ="intro"><h1>Start cooking with PocketChef!</h1></div>
-        <img className="land_bkg" src={logo}/>
+        <div className="intro"><h1>Start cooking with PocketChef!</h1></div>
+        <img className="land_bkg" src={logo} />
       </div>
     )
   }
