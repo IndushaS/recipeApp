b8ccec6e3cea142dd85f435dbdce17524341181d
Hamza <hamzakazmi10@gmail.com>
2020-12-06 22:25:28 -0500

    Added Recipe cards to recommendation view

--- src/components/Recommendation.js
+++ src/components/Recommendation.js
@@ -4,116 +4,115 @@ import JSONPretty from 'react-json-pretty';
 import RecipeCard from "./RecipeCard";
 import { Row, Col } from 'react-simple-flex-grid';
 import "react-simple-flex-grid/lib/main.css";
-
-import { useIsMounted } from 'react-tidy'
+import "./Recommendation.css";
+import {useIsMounted} from 'react-tidy'
 
 
 //Recommendation component
 const Recommendations = () => {
-  const { user, isAuthenticated } = useAuth0();
-  const [hasError, setErrors] = useState(false);
-  const [recommendations, setRecommendations] = useState([]);
-  const [id, setId] = useState();
-  const [recipes, setRecipes] = useState();
+    const { user, isAuthenticated } = useAuth0();
+    const [hasError, setErrors] = useState(false);
+    const [recommendations, setRecommendations] = useState([]);
+    const [id, setId] = useState();
+    const [recipes, setRecipes] = useState();
 
 
 
-  async function fetchData() {
-    const res = await fetch("https://recipe342backend.herokuapp.com/getRecommendations?id=3");
-    res
-      .json()
-      .then(res => setRecommendations(res))
-      .catch(err => setErrors(err));
-  }
+    async function fetchData() {
+        const res = await fetch("https://recipe342backend.herokuapp.com/getRecommendations?id=3");
+        res
+            .json()
+            .then(res => setRecommendations(res))
+            .catch(err => setErrors(err));
+    }
 
-  async function getRecipes() {
-    const res = await fetch("https://recipe342backend.herokuapp.com/getrecipes");
-    res
-      .json()
-      .then(res => setRecipes(res))
-      .catch(err => setErrors(err));
-  }
-
-
-  async function getUserid() {
-    const result = await fetch("https://recipe342backend.herokuapp.com/getUserid?email=indusha999@gmail.com");
-    result
-      .json()
-      .then(result => setId(result))
-      .catch(error => setErrors(error));
-  }
-
-
-  const isMounted = useIsMounted()
-
-  React.useEffect(() => {
-    fetchData().then((result) => {
-      if (isMounted) {
-        setRecommendations(result)
+    async function getRecipes() {
+        const res = await fetch("https://recipe342backend.herokuapp.com/getrecipes");
+        res
+          .json()
+          .then(res => setRecipes(res))
+          .catch(err => setErrors(err));
       }
-    })
-
-    getRecipes().then((result) => {
-      if (isMounted) {
-        setRecipes(result)
-      }
-    })
-  }, [])
 
 
+    async function getUserid() {
+        const result = await fetch("https://recipe342backend.herokuapp.com/getUserid?email=indusha999@gmail.com");
+        result
+            .json()
+            .then(result => setId(result))
+            .catch(error => setErrors(error));
+    }
 
 
-  //console.log(recipes && recipes.filter((recipe) => {return recommendations && recipe.recipeName == recommendations[0]}));
+    const isMounted = useIsMounted()
+
+    React.useEffect(() => {
+      fetchData().then((result) => {
+        if (isMounted) {
+          setRecommendations(result)
+        }
+      })
+
+      getRecipes().then((result) => {
+        if (isMounted) {
+          setRecipes(result)
+        }
+      })
+    }, [])
 
 
-  let filteredRecipes = recommendations && recipes && recipes.filter(function (o1) {
-    return recommendations.some(function (o2) {
-      return o1.recipeName === o2; // return the ones with equal id
-    });
+
+
+    //console.log(recipes && recipes.filter((recipe) => {return recommendations && recipe.recipeName == recommendations[0]}));
+
+
+    let filteredRecipes = recommendations && recipes && recipes.filter(function (o1) {
+      return recommendations.some(function (o2) {
+          return o1.recipeName === o2; // return the ones with equal id
+     });
   });
+  
 
 
 
 
+    //If user is logged in, it displays user information
+    return (
+        true && (
 
-  //If user is logged in, it displays user information
-  return (
-    true && (
-
-
-
-
-      <div>
-        <img src={"https://lh3.googleusercontent.com/ogw/ADGmqu90MCDtF1tR1J6YYOvblwW-zp641R_BfzKlpKgrvA=s64-c-mo"} />
-        <h2>Recommendations for indusha</h2>
+          
 
 
+            <div>
+                <img src={"https://lh3.googleusercontent.com/ogw/ADGmqu90MCDtF1tR1J6YYOvblwW-zp641R_BfzKlpKgrvA=s64-c-mo"} />
+                <h2>Recommendations for indusha</h2>
+         
+        
         <hr />
 
 
 
-        <Row gutter={40}>
-          {filteredRecipes && filteredRecipes.map((
-            recipe) => (
+                <Row gutter={40}>
+            {filteredRecipes && filteredRecipes.map((
+              recipe) => (
 
-              <Col
-                xs={{ span: 6 }} sm={{ span: 4 }} md={{ span: 4 }}
-                lg={{ span: 3 }} xl={{ span: 3 }}>
+                <Col
+                  xs={{ span: 6 }} sm={{ span: 4 }} md={{ span: 4 }}
+                  lg={{ span: 3 }} xl={{ span: 3 }}>
 
-                <RecipeCard name={recipe.recipeName} id={recipe.idrecipe} instructions={recipe.recipeInstruction} img={recipe.imgURL} ingredients={recipe.ingredients} numLikes={recipe.totalLikes}
-                  likeStatus={recipe.likedStatus} />
-
+                  <RecipeCard name={recipe.recipeName} id={recipe.idrecipe} instructions={recipe.recipeInstruction} img={recipe.imgURL} ingredients={recipe.ingredients}  />
 
 
-              </Col>
 
-            ))}
-        </Row>
+                </Col>
+
+              ))}
+          </Row>
 
 
-      </div>
+            </div>
+        )
     )
-  )
 }
 
 export default Recommendations
